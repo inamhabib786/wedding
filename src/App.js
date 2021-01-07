223d7bce885d2af3ffd53e18be94853f8128cbc9
inamkloudlearn <inam@kloudlearn.com>
2021-01-07 09:08:46 +0530

    yarn added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import invite from "./WeddingInviteKhan.jpg";
+
+function App() {
+  return (
+    <div class="pyro">
+      <div class="before"></div>
+      <div class="after"></div>
+      <div className="container">
+        <img src={invite} className="invite" alt="WahidSumaila"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
